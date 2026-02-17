646af535862433cfe1abe6519dd7a57c92bbed2a
kzoeps <kzoepa@gmail.com>
2026-02-18 00:13:37 +0600

    Add RPC_SCOPE back to granular OAuth scope (hypercerts-bkw)

--- lib/config.ts
+++ lib/config.ts
@@ -33,9 +33,9 @@ const REPO_COLLECTIONS = [
 ];
 
 const HYPERCERT_REPO_SCOPE = `repo?${REPO_COLLECTIONS.map((c) => "collection=" + c).join("&")}&action=create&action=update&action=delete`;
+const BLOB_SCOPE = "blob:*/*";
 const RPC_SCOPE =
   "rpc:app.bsky.actor.getProfile?aud=did:web:api.bsky.app%23bsky_appview";
-const BLOB_SCOPE = "blob:*/*";
 
 const GRANULAR_SCOPE = [
   ATPROTO_SCOPE,
