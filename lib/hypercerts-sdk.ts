805c9cd8d1ab6c4c4c2ab6375b2213d0824cc504
kzoeps <kzoepa@gmail.com>
2026-02-17 19:25:40 +0600

    Make OAUTH_SCOPE environment-aware: granular for production, loopback for dev (hypercerts-02l.2)

--- lib/hypercerts-sdk.ts
+++ lib/hypercerts-sdk.ts
@@ -7,16 +7,6 @@ export const stateStore = new RedisStateStore();
 
 export { OAUTH_SCOPE };
 
-// Future granular scopes (not yet used)
-export const HYPERCERT_REPO_SCOPE =
-  "repo?collection=org.hypercerts.claim.activity&collection=org.hypercerts.claim.contribution&collection=org.hypercerts.claim.evaluation&collection=org.hypercerts.claim.evidence&collection=org.hypercerts.claim.measurement&collection=org.hypercerts.claim.collection&collection=org.hypercerts.claim.rights&collection=org.hypercerts.claim.funding&collection=app.certified.location&action=create&action=update&action=delete";
-
-export const RPC_SCOPE = [
-  "rpc:app.bsky.actor.getProfile?aud=did:web:api.bsky.app%23bsky_appview",
-].join(" ");
-
-export const BLOB_SCOPE = "blob?accept=video/*&accept=image/*";
-
 // OAuth configuration using centralized config
 const oauthConfig = {
   clientId: config.clientId,
