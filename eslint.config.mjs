621f391e47b4e4baf169c5e2360bcaa8116d25f4
kzoeps <kzoepa@gmail.com>
2026-02-26 22:28:18 +0600

    Add eslint-plugin-react-compiler in warn mode (hypercerts-ecf.2)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,11 +3,18 @@ import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 import prettierConfig from "eslint-config-prettier";
 import pluginQuery from "@tanstack/eslint-plugin-query";
+import reactCompiler from "eslint-plugin-react-compiler";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   ...pluginQuery.configs["flat/recommended"],
+  reactCompiler.configs.recommended,
+  {
+    rules: {
+      "react-compiler/react-compiler": "warn",
+    },
+  },
   prettierConfig,
   // Override default ignores of eslint-config-next.
   globalIgnores([
