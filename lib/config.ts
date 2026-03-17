d2892c3fbaadb050091be8c682e853251556925d
kzoeps <kzoepa@gmail.com>
2026-03-17 16:13:41 +0600

    revert

--- lib/config.ts
+++ lib/config.ts
@@ -242,7 +242,6 @@ export const config = {
 
   // ePDS handle mode configuration
   epdsHandleMode: (() => {
-    return undefined;
     const mode =
       process.env.NEXT_PUBLIC_EPDS_HANDLE_MODE || "picker-with-random";
     if (!(EPDS_HANDLE_MODES as readonly string[]).includes(mode)) {
